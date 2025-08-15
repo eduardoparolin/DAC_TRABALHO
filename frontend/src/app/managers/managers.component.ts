6c1f299d9cf6dfb31725a58e627bff87366318ff
eduardoparolin <eduparolin@gmail.com>
2025-08-15 09:32:44 -0300

    chore: main pages components creation

---
+++ frontend/src/app/managers/managers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managers',
+  imports: [],
+  templateUrl: './managers.component.html',
+  styleUrl: './managers.component.scss'
+})
+export class ManagersComponent {
+
+}
