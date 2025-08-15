5a676d2974056a67455f6fc54d8996b889dbb04e
eduardoparolin <eduparolin@gmail.com>
2025-08-15 09:38:29 -0300

    chore: route test: OK; material test: OK

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {ClientsComponent} from './clients/clients.component';
+import {ManagersComponent} from './managers/managers.component';
+import {AccountsComponent} from './accounts/accounts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'clients',
+    component: ClientsComponent
+  },
+  {
+    path: 'managers',
+    component: ManagersComponent
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent
+  }
+];
