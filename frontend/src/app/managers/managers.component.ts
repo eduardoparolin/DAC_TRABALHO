e4eaf91f87ba8c241acd67a8b138ea18fb1ee061
eduardoparolin <eduparolin@gmail.com>
2025-08-30 17:09:03 -0300

    feat: add new manager dialog with form validation and action buttons

--- frontend/src/app/managers/managers.component.ts
+++ frontend/src/app/managers/managers.component.ts
@@ -6,23 +6,42 @@ import {ManagersService} from './managers.service';
 import {MatIconModule} from '@angular/material/icon';
 import {ConfirmationDialogComponent} from '../utils/confirmation-dialog/confirmation-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
+import {MatButton} from '@angular/material/button';
+import {NewEditManagerDialogComponent} from './new-edit-manager-dialog/new-edit-manager-dialog.component';
+import {Manager} from './manager.model';
 
 @Component({
   selector: 'app-managers',
-  imports: [MatTableModule, MatIconModule],
+  imports: [MatTableModule, MatIconModule, MatButton],
   templateUrl: './managers.component.html',
   styleUrl: './managers.component.scss'
 })
 export class ManagersComponent implements OnInit {
-    service = inject(ManagersService);
-    readonly dialog = inject(MatDialog);
-    displayedColumns: string[] = ['NAME', 'CPF', 'EMAIL', 'PHONE', 'ACTIONS'];
+  service = inject(ManagersService);
+  readonly dialog = inject(MatDialog);
+  displayedColumns: string[] = ['NAME', 'CPF', 'EMAIL', 'PHONE', 'ACTIONS'];
 
-    ngOnInit() {
-      this.service.getAllManagers();
-    }
+  ngOnInit() {
+    this.service.getAllManagers();
+  }
 
-    deleteManager(id: number) {
-      this.dialog.open(ConfirmationDialogComponent, {width: '250px'});
-    }
+  deleteManager(id: number) {
+    this.dialog.open(ConfirmationDialogComponent, {width: '250px'});
+  }
+
+  newManager() {
+    this.dialog.open(NewEditManagerDialogComponent, {
+      data: {
+        manager: null
+      }
+    });
+  }
+
+  editManager(manager: Manager) {
+    this.dialog.open(NewEditManagerDialogComponent, {
+      data: {
+        manager: manager
+      }
+    });
+  }
 }
--- frontend/src/app/managers/new-edit-manager-dialog/new-edit-manager-dialog.component.ts
+++ frontend/src/app/managers/new-edit-manager-dialog/new-edit-manager-dialog.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-edit-manager-dialog',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatButton
+  ],
   templateUrl: './new-edit-manager-dialog.component.html',
   styleUrl: './new-edit-manager-dialog.component.scss'
 })
 export class NewEditManagerDialogComponent {
+  dialog = inject(MatDialogRef)
+  cpfFormControl = new FormControl(null, [Validators.required]);
+  nameFormControl = new FormControl(null, [Validators.required]);
+  emailFormControl = new FormControl(null, [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl(null, [Validators.required]);
 
+  cancel() {
+    this.dialog.close();
+  }
+
+  save() {
+    if (this.cpfFormControl.valid && this.nameFormControl.valid && this.emailFormControl.valid && this.passwordFormControl.valid) {
+      // Save logic here
+      this.dialog.close();
+    }
+  }
 }
