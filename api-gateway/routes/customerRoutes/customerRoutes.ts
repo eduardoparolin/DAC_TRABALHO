dc0ecc23492ff1e3a5339b0b56dba9c575b7ad59
Milton Matias Júnior <miltonmatiasj@gmail.com>
2025-09-01 16:12:55 -0300

    feat: declare customer endpoints

--- api-gateway/routes/customerRoutes/customerRoutes.ts
+++ api-gateway/routes/customerRoutes/customerRoutes.ts
@@ -1,3 +1,45 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import {
+  aproveCustomerByCPFSchemaInput,
+  createCustomerSchemaInput,
+  declineCustomerByCPFSchemaInput,
+  getCustomerByCPFSchemaInput,
+  getCustomersSchemaInput,
+  UpdateCustomerByCPFSchemaInput,
+} from "./customerRoutesSchema";
 
 export const customerRoutes = new Hono();
+
+customerRoutes.get(
+  "/",
+  zValidator("json", getCustomersSchemaInput, async (c) => {})
+);
+
+customerRoutes.get(
+  "/:cpf",
+  zValidator("json", getCustomerByCPFSchemaInput, async (c) => {})
+);
+
+customerRoutes.post(
+  "/",
+  zValidator("json", createCustomerSchemaInput),
+  async (c) => {}
+);
+
+customerRoutes.put(
+  "/:cpf",
+  zValidator("json", UpdateCustomerByCPFSchemaInput, async (c) => {})
+);
+
+customerRoutes.post(
+  "/:cpf/aprovar",
+  zValidator("json", aproveCustomerByCPFSchemaInput),
+  async (c) => {}
+);
+
+customerRoutes.post(
+  "/:cpf/reprovar",
+  zValidator("json", declineCustomerByCPFSchemaInput),
+  async (c) => {}
+);
