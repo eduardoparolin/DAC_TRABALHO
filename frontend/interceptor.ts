b63b5340a4bbafa619f7cc5dae69cb209cb9d2d5
eduardoparolin <eduparolin@gmail.com>
2025-09-22 16:57:06 -0300

    feat: implement address handling in signup process with form validation

---
+++ frontend/interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+// import { AuthService } from './authentication/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.url.includes('login') || req.url.includes('signup')) {
+    return next(req);
+  }
+
+  // const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const authToken = '';
+  const authReq = req.clone({
+    setHeaders: { Authorization: `Bearer ${authToken}` },
+  });
+  return next(authReq).pipe(
+    catchError((error) => {
+      if (error.status === 401 || error.status === 403) {
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
+};
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,9 +1,11 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatButton} from '@angular/material/button';
-import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatError, MatFormField, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
 import {MatProgressBar} from '@angular/material/progress-bar';
 import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {SignupService} from './signup.service';
+import { NgxMaskDirective } from 'ngx-mask';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-signup',
@@ -14,15 +16,42 @@ import {SignupService} from './signup.service';
     MatInput,
     MatError,
     MatButton,
-    MatProgressBar
+    MatProgressBar,
+    NgxMaskDirective,
+    MatSuffix,
+    MatProgressSpinner,
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   service = inject(SignupService);
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passwordFormControl = new FormControl('', [Validators.required]);
+  cpfFormControl = new FormControl('', [Validators.required]);
+  phoneFormControl = new FormControl('', [Validators.required]);
+  ruaFormControl = new FormControl('', [Validators.required]);
+  bairroFormControl = new FormControl('', [Validators.required]);
+  cidadeFormControl = new FormControl('', [Validators.required]);
+  estadoFormControl = new FormControl('', [Validators.required]);
+  numeroFormControl = new FormControl('', [Validators.required]);
+  complementoFormControl = new FormControl('', []);
+  cepFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  nameFormControl = new FormControl('', [Validators.required]);
+
+  ngOnInit(): void {
+    this.cepFormControl.valueChanges.subscribe(() => {
+      if (this.cepFormControl.invalid) return;
+      this.service.fetchAddress(this.cepFormControl.value!).then(address => {
+        console.log(address);
+        if (address) {
+          this.ruaFormControl.setValue(address.street);
+          this.bairroFormControl.setValue(address.neighborhood);
+          this.cidadeFormControl.setValue(address.city);
+          this.estadoFormControl.setValue(address.state);
+        }
+      });
+    })
+  }
 
   signup() {
 
