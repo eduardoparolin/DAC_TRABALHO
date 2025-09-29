25aa1116d1cf94e36e208af855af6c4742a35087
Milton Matias Júnior <miltonmatiasj@gmail.com>
2025-09-28 22:02:23 -0300

    fix: add phone number to managers screen

--- frontend/src/app/managers/manager.model.ts
+++ frontend/src/app/managers/manager.model.ts
@@ -1,17 +1,38 @@
-import {Client} from '../clients/client.model';
-import {User, UserType} from '../session/user.model';
+import { Client } from '../clients/client.model';
+import { User, UserType } from '../session/user.model';
 
 export class Manager extends User {
   clientList: Client[] = [];
+  phone: string;
 
-  constructor(id: number, name: string, email: string, cpf: string, type: UserType, clientList: Client[]) {
+  constructor(
+    id: number,
+    name: string,
+    email: string,
+    cpf: string,
+    type: UserType,
+    clientList: Client[],
+    phone: string
+  ) {
     super(id, name, email, cpf, type);
     this.clientList = clientList;
+    this.phone = phone;
   }
 
   static override fromJson(json: any): Manager {
-    const clients = (json['clientes'] as any[])?.map(clientJson => Client.fromJson(clientJson)) ?? [];
-    return new Manager(json['id'], json['nome'], json['email'], json['cpf'], json['tipo'], clients);
+    const clients =
+      (json['clientes'] as any[])?.map((clientJson) =>
+        Client.fromJson(clientJson)
+      ) ?? [];
+    return new Manager(
+      json['id'],
+      json['nome'],
+      json['email'],
+      json['cpf'],
+      json['tipo'],
+      clients,
+      json['phone']
+    );
   }
 
   get totalBalance(): number {
--- frontend/src/app/managers/managers.service.ts
+++ frontend/src/app/managers/managers.service.ts
@@ -1,23 +1,25 @@
-import {inject, Injectable, signal} from '@angular/core';
-import {lastValueFrom} from 'rxjs';
-import {environment} from '../../environments/environment';
-import {HttpClientMockService} from '../utils/http-client-mock.service';
-import {GetManagersResponse} from './manager.types';
-import {Manager} from './manager.model';
+import { inject, Injectable, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClientMockService } from '../utils/http-client-mock.service';
+import { GetManagersResponse } from './manager.types';
+import { Manager } from './manager.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ManagersService {
   // http = inject(HttpClient);
   http = inject(HttpClientMockService);
   managers = signal<Manager[]>([]);
 
-  constructor() { }
+  constructor() {}
 
   async getAllManagers() {
-    const managersResponse = await lastValueFrom(this.http.get<GetManagersResponse[]>(`${environment.baseUrl}/gerentes`))
-    const managers = managersResponse.map(m => Manager.fromJson(m));
+    const managersResponse = await lastValueFrom(
+      this.http.get<GetManagersResponse[]>(`${environment.baseUrl}/gerentes`)
+    );
+    const managers = managersResponse.map((m) => Manager.fromJson(m));
     this.managers.set(managers);
     return managers;
   }
