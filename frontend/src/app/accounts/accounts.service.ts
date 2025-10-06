990f2327f756e3c6d01a2588cca9dbb3db44a8fa
eduardoparolin <eduparolin@gmail.com>
2025-10-06 18:57:47 -0300

    feat: implement accounts table with client approval actions and add card component

--- frontend/src/app/accounts/accounts.service.ts
+++ frontend/src/app/accounts/accounts.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {ClientApprovalService} from '../client-approval/client-approval.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
 
-  constructor() { }
+  service = inject(ClientApprovalService)
+
+  displayedColumns: string[] = ['CPF', 'NAME', 'EMAIL', 'WAGE', 'ACTIONS'];
+
+  ngOnInit() {
+    this.service.getAllClients();
+  }
+
+  approveClient(clientId: string) {
+    this.service.approveClient(clientId);
+  }
+
+  rejectClient(clientId: string) {
+    this.service.rejectClient(clientId);
+  }
 }
