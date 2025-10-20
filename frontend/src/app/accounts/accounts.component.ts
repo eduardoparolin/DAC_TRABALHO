93952d921a7cea05efae88aacf5d1969b943b79f
felipebassetti <bassettifelipe@gmail.com>
2025-10-20 18:39:31 -0300

    fix(client-component): the service imports and methods were wrong.

--- frontend/src/app/accounts/accounts.component.ts
+++ frontend/src/app/accounts/accounts.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {
     MatCell,
@@ -9,8 +9,6 @@ import {
     MatHeaderRowDef,
     MatRow, MatRowDef, MatTable
 } from "@angular/material/table";
-import {AccountsService} from './accounts.service';
-import {ClientApprovalService} from '../client-approval/client-approval.service';
 
 @Component({
   selector: 'app-accounts',
@@ -30,80 +28,5 @@ import {ClientApprovalService} from '../client-approval/client-approval.service'
   styleUrl: './accounts.component.scss'
 })
 export class AccountsComponent {
-  service = inject(ClientApprovalService)
+
 }
-//
-// import {Component, inject} from '@angular/core';
-// import {MatButton} from "@angular/material/button";
-// import {
-//   MatCell,
-//   MatCellDef,
-//   MatColumnDef,
-//   MatHeaderCell,
-//   MatHeaderRow,
-//   MatHeaderRowDef,
-//   MatRow, MatRowDef, MatTable
-// } from "@angular/material/table";
-// import {AccountsService} from './accounts.service';
-// import {ClientApprovalService} from '../client-approval/client-approval.service';
-//
-// @Component({
-//   selector: 'app-accounts',
-//   imports: [
-//     MatButton,
-//     MatCell,
-//     MatCellDef,
-//     MatColumnDef,
-//     MatHeaderCell,
-//     MatHeaderRow,
-//     MatHeaderRowDef,
-//     MatRow,
-//     MatRowDef,
-//     MatTable
-//   ],
-//   templateUrl: './accounts.component.html',
-//   styleUrl: './accounts.component.scss'
-// })
-// export class AccountsComponent {
-//   service = inject(ClientApprovalService)
-// }
-//
-//
-// import {Component, inject} from '@angular/core';
-// import {MatButton} from "@angular/material/button";
-// import {
-//   MatCell,
-//   MatCellDef,
-//   MatColumnDef,
-//   MatHeaderCell,
-//   MatHeaderRow,
-//   MatHeaderRowDef,
-//   MatRow, MatRowDef, MatTable
-// } from "@angular/material/table";
-// import {AccountsService} from './accounts.service';
-// import {ClientApprovalService} from '../client-approval/client-approval.service';
-//
-// @Component({
-//   selector: 'app-accounts',
-//   imports: [
-//     MatButton,
-//     MatCell,
-//     MatCellDef,
-//     MatColumnDef,
-//     MatHeaderCell,
-//     MatHeaderRow,
-//     MatHeaderRowDef,
-//     MatRow,
-//     MatRowDef,
-//     MatTable
-//   ],
-//   templateUrl: './accounts.component.html',
-//   styleUrl: './accounts.component.scss'
-// })
-// export class AccountsComponent {
-//   service = inject(ClientApprovalService)
-// }
-//
-
-
-//REMOVER TUDO ACIMA, ESTE COMPONENTE PRECISA DE REVISAO
