bb151284e1ea77920a8a1ced320c00bb20850c01
Rafael <rafaelleonardodasilva.dv@gmail.com>
2025-10-26 12:13:34 -0300

    feat: manager container

---
+++ manager/src/main/resources/db/migration/V1__create-table-manager.sql
@@ -0,0 +1,8 @@
+CREATE TABLE manager (
+     id BIGSERIAL PRIMARY KEY,
+     cpf VARCHAR(14) NOT NULL,
+     name VARCHAR(255) NOT NULL,
+     email VARCHAR(255) NOT NULL UNIQUE,
+     password VARCHAR(255) NOT NULL,
+     type VARCHAR(50) NOT NULL
+);
